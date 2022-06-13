00f1d7a416e25b3313891b3b8e3292af4ffdc1ee
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-06-13 14:54:16 +0100

    External links added

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -19,22 +19,26 @@ const Navbar = () => {
         </a>
       </div>
       <div className="flex-1">
-        <Image
-          alt="opensea"
-          src={openseaLogo}
-          width={32}
-          height={32}
-          className="cursor-pointer"
-        />
+        <a href="https://testnets.opensea.io/collection/weirdcreatures-v4">
+          <Image
+            alt="opensea"
+            src={openseaLogo}
+            width={32}
+            height={32}
+            className="cursor-pointer"
+          />
+        </a>
       </div>
       <div className="flex-1">
-        <Image
-          alt="etherscan"
-          src={etherscanLogo}
-          width={32}
-          height={32}
-          className="cursor-pointer"
-        />
+        <a href="https://rinkeby.etherscan.io/address/0x11ecef94728fb5048c1db845f34be160ed5aae51">
+          <Image
+            alt="etherscan"
+            src={etherscanLogo}
+            width={32}
+            height={32}
+            className="cursor-pointer"
+          />
+        </a>
       </div>
     </div>
   );
